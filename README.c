35ef4396e67b238da851bc65b44e190f1b7722d8
dandapataishwarya <dandapataishwarya@gmail.com>
2016-07-08 15:25:01 +0530

    finish

---
+++ README.c
@@ -0,0 +1,27 @@
+# largest
+#include<stdio.h>
+void main()
+{
+int n1,n2,n3,max;
+printf("enter three numbers");
+scanf("%d %d %d",&n1,&n2,&n3);
+max=n1;
+if(max>n2)
+{
+  if(max>n3)
+  {
+  printf("%d is largest",&n1);
+  }
+}  
+  else if(n2>n3)
+  {
+  printf("%d is largest",&n2);
+  }
+  else
+  {
+  printf("%d is largest",&n3);
+  }
+ }
+}
+
+   
